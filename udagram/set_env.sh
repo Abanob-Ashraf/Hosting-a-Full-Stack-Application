61a8c7668d05486e5db3909f37467ff648b5026c
Abanob-Ashraf <abanobashraf74@gmail.com>
2022-11-03 06:42:55 +0200

    init commit

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_DB=postgres
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
 export POSTGRES_HOST=database-1.crmvgdfjqunw.us-east-2.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_MEDIA_BUCKET=abanob-udagram
 export AWS_DEFAULT_REGION=us-east-2
 export AWS_PROFILE=default
+export AWS_MEDIA_BUCKET=abanob-udagram
 export JWT_SECRET=your-secret-token
 export URL=http://udagram-api-dev.eba-e7wei2vd.us-east-2.elasticbeanstalk.com/
